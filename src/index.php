21a94f8631934fed43a2bc13e97802e15485e9a7
Thiago <thiago.larangeira50@gmail.com>
2024-06-06 14:26:43 -0300

    atualizando chamadas de funcoes

--- src/index.php
+++ src/index.php
@@ -1,34 +1,37 @@
 <?php
-//var_dump($_REQUEST); // print any variable to echo html
-//phpinfo(); // show all info 
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    exit(0);
-}
-
-session_start();
-
-require_once("./models/models.php");
-require_once("./repositories/repo.php");
-require_once("./controllers/authController.php");
-require_once("./controllers/userController.php");
-require_once("./controllers/ouvidoriaController.php");
-require_once("./controllers/anexoController.php");
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Credentials: true");
 
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+if(!isset($_SERVER["PATH_INFO"])){
+    readfile("./views/index.html");
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
 
 
 try {    
+    session_start();
+
+    require_once("./models/models.php");
+    require_once("./repositories/repo.php");
+    require_once("./controllers/authController.php");
+    require_once("./controllers/userController.php");
+    require_once("./controllers/ouvidoriaController.php");
+    require_once("./controllers/anexoController.php");
+
     $controllers = [
-        "/login" => "authController",
-        "/signin" => "authController",
+        "/login" => "loginController",
+        "/signin" => "signinController",
         "/user" => "userController",
         "/ouvidoria" => "ouvidoriaController",
         "/ouvidoria/anexos" => "ouvidoriaAnexosController"
@@ -41,5 +44,5 @@ try {
 } catch (Throwable $th) {
     http_response_code(500);
     header('Content-Type: application/json');
-    echo json_encode(["error" => $th->getMessage()]);
+    echo json_encode(["error" => $th->__toString()]);
 }
